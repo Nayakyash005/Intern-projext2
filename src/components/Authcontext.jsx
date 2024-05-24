3454bc407994e31ad565c9aaa30f305e81ee9313
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-05-24 17:40:10 +0530

    sdv

---
+++ src/components/Authcontext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true); // <-- Add a loading state
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedUser = jwtDecode(token);
+      setUser(decodedUser);
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem(token);
+    const decodedUser = jwtDecode(token);
+    setUser(decodedUser);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
